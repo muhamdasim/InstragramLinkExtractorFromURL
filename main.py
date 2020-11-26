7dc8e2e593c2c54d153b27b877a45023f3ad2886
Muhammad Asim <47195175+muhamdasim@users.noreply.github.com>
2020-11-26 17:43:42 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+import csv
+import re
+
+#change the filename below
+df=pd.read_csv('xyz.csv')
+
+
+def getLinks(url):
+    try:
+        html_page = requests.get(url,timeout=60)
+        soup = str(BeautifulSoup(html_page.text,'lxml'))
+        urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', soup)
+        for i in urls:
+            if 'instagram' in i:
+                return i
+
+    except:
+        return -1
+
+    return -1
+
+insta_urls=[]
+url=[]
+for i in df['url']:
+    url.append(i)
+    print("Extracting",i)
+    insta_urls.append(getLinks(i))
+
+
+
+with open("output.csv", "w",newline='',encoding="utf-8") as csvFile:
+    fieldnames = ['url','Instagram']
+    writer = csv.DictWriter(csvFile, fieldnames=fieldnames)
+    writer.writeheader()
+    for u,insta in zip(url,insta_urls):
+        writer.writerow({'url':u,'Instagram':insta})
